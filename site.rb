0ce91035b42d73178143d172427e10da63fe1240
Nat Welch <nat@natwelch.com>
2016-02-17 01:49:06 +0000

    init site

---
+++ site.rb
@@ -0,0 +1,47 @@
+require "rubygems"
+require "bundler"
+Bundler.require(:default, ENV["RACK_ENV"] || :development)
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
+
+class Inspiration < Sinatra::Base
+  register ScssInitializer
+  use Rack::Deflater
+
+  layout :main
+  configure do
+    RACK_ENV = (ENV['RACK_ENV'] || :development).to_sym
+    connections = {
+      :development => "postgres://localhost/happynat",
+      :test => "postgres://postgres@localhost/happynat_test",
+      :production => ENV['DATABASE_URL']
+    }
+
+    url = URI(connections[RACK_ENV])
+    options = {
+      :adapter => url.scheme,
+      :host => url.host,
+      :port => url.port,
+      :database => url.path[1..-1],
+      :username => url.user,
+      :password => url.password
+    }
+
+    case url.scheme
+    when "sqlite"
+      options[:adapter] = "sqlite3"
+      options[:database] = url.host + url.path
+    when "postgres"
+      options[:adapter] = "postgresql"
+    end
+    set :database, options
+
+    use Rack::Session::Cookie, :key => 'rack.session',
+      :path => '/',
+      :expire_after => 86400, # 1 day
+      :secret => ENV['SESSION_SECRET'] || '*&(^B234'
+  end
+
+  get "/" do
+    erb :index
+  end
+end
